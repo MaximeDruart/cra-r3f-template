aad7236126638be61fd138408c182d48780bec32
eythanSaillet <eythan.saillet@gmail.com>
2020-12-14 22:14:38 +0100

    :bug: fix useResource bug in lightWrappers

--- src/utils/lightWrappers.js
+++ src/utils/lightWrappers.js
@@ -2,23 +2,23 @@ import React from 'react'
 import { useResource } from 'react-three-fiber'
 
 const PointLightWrapper = ({ position, intensity = 1, distance = 0, color = 0xffffff, helper = false }) => {
-	const [pointLightRef, pointLight] = useResource()
+	const pointLightRef = useResource()
 
 	return (
 		<>
-			<pointLight position={position} intensity={intensity} distance={distance} color={color} decay={2} ref={pointLightRef} />
-			{helper && pointLight && <pointLightHelper args={[pointLight]} />}
+			{<pointLight position={position} intensity={intensity} distance={distance} color={color} decay={2} ref={pointLightRef} />}
+			{helper && pointLightRef.current && <pointLightHelper args={[pointLightRef.current]} />}
 		</>
 	)
 }
 
-const SpotLightWrapper = ({ position, intensity = 1, distance = 0, color = 0xffffff, angle = Math.PI / 2, penumbra = 0, helper = false }) => {
-	const [spotLightRef, spotLight] = useResource()
+const SpotLightWrapper = ({ position, intensity = 1, distance = 0, color = 0xffffff, angle = 0.5, penumbra = 0, helper = false }) => {
+	const spotLightRef = useResource()
 
 	return (
 		<>
-			<spotLight position={position} intensity={intensity} distance={distance} color={color} decay={2} angle={angle} penumbra={penumbra} ref={spotLightRef} />
-			{helper && spotLight && <spotLightHelper args={[spotLight]} />}
+			{<spotLight position={position} intensity={intensity} distance={distance} color={color} decay={2} angle={angle} penumbra={penumbra} ref={spotLightRef} />}
+			{helper && spotLightRef.current && <spotLightHelper args={[spotLightRef.current]} />}
 		</>
 	)
 }
